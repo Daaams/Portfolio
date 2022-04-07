bffb0d7adba5472b625e6e77615460f9d863e2cb
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2022-04-07 18:40:01 +0200

    site amélioré

--- pages/menu.js
+++ pages/menu.js
@@ -2,8 +2,75 @@ function openNav() {
     document.getElementById("sideNavigation").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
 }
- 
+
 function closeNav() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+}
+var l = 0;
+
+function decalageGauche() {
+    var item = document.getElementById('conteneur');
+    var windowWidth = window.innerWidth;
+    l--;
+    if (windowWidth <= 900) {
+        switch (l) {
+            case l = 0:
+                item.style.marginLeft = "0%";
+                break;
+            case l = 1:
+                item.style.marginLeft = "-70%"
+                break;
+            case l = 2:
+                item.style.marginLeft = "-140%"
+                break;
+            default:
+                l = 0;
+        }
+    }
+    if (windowWidth > 900) {
+        switch (l) {
+            case l = 0:
+                item.style.marginLeft = "0%";
+                break;
+            case l = 1:
+                item.style.marginLeft = "-70%"
+                break;
+            default:
+                l = 0;
+        }
+    }
+}
+
+function decalageDroite() {
+    var item = document.getElementById('conteneur');
+    var windowWidth = window.innerWidth;
+    l++;
+    if (windowWidth <= 900) {
+        switch (l) {
+            case l = 0:
+                item.style.marginLeft = "0%";
+                break;
+            case l = 1:
+                item.style.marginLeft = "-70%"
+                break;
+            case l = 2:
+                item.style.marginLeft = "-140%"
+                break;
+            default:
+                l = 2;
+        }
+    }
+    if (windowWidth > 900) {
+        switch (l) {
+            case l = 0:
+                item.style.marginLeft = "0%";
+                break;
+            case l = 1:
+                item.style.marginLeft = "-70%"
+                break;
+            default:
+                l = 1;
+        }
+    }
 }
