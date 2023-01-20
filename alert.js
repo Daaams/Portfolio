b1dcfd38c5180737a82296629c9554ef8edd175a
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2023-01-20 09:51:37 +0100

    alert new portfolio

---
+++ alert.js
@@ -0,0 +1,9 @@
+const link = "https://portfolio-fawn-iota-73.vercel.app/";
+const link2 = "https://daaams.github.io/Portfolio/";
+
+const message = "Click OK button to visit my new portfolio";
+if (confirm(message)){
+    window.location.href = link
+}else{
+    window.location.href = link2;
+}
