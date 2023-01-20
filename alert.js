790e5b0046cd74d1a97838a7a9a5ce2440a13f8a
dlevrault <damien.levrault@etu.u-bordeaux.fr>
2023-01-20 09:55:13 +0100

    fix bug

--- alert.js
+++ alert.js
@@ -4,6 +4,4 @@ const link2 = "https://daaams.github.io/Portfolio/";
 const message = "Click OK button to visit my new portfolio";
 if (confirm(message)){
     window.location.href = link
-}else{
-    window.location.href = link2;
 }
